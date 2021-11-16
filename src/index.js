7c8df907e81ffb974ed0fda1eb80318f79ef302a
Kretsul-Vitaliy <kreculvitalii@gmail.com>
2021-11-16 22:16:22 +0200

    add info task1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+// export default function getRefs() {
+//   return {
+//     input: document.querySelector('#search-box'),
+//     countryList: document.querySelector('.country-list'),
+//     countryInfo: document.querySelector('.country-info'),
+//   };
+// }
